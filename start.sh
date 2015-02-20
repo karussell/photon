cd474b643c97a80cc0cc3e30d5f8d4ac08fb8d2d
Peter <peathal@yahoo.de>
2015-02-20 17:44:23 +0100

    added xmx to start script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 cd /install/photon
-java -jar target/photon-0.2.2-SNAPSHOT.jar -data-dir /data/osm/ -listen-port 5001 -listen-ip 136.243.16.13
+java -Xms15000m -Xmx15000m -jar target/photon-0.2.2-SNAPSHOT.jar -data-dir /data/osm/ -listen-port 5001 -listen-ip 136.243.16.13
